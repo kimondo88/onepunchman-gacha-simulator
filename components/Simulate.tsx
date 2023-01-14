930856015a15f86c5399a4b83bc8bbc452b72601
kimondo88 <kimondo1988@o2.pl>
2023-01-14 13:31:01 +0100

    remove useless memo, change to select string to owlboros

--- components/Simulate.tsx
+++ components/Simulate.tsx
@@ -1,13 +1,10 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import Coins from "./Coins";
 import Select from "./Select";
 export default function Simulate(){
     const [coins, setCoins] = useState(10);
-    const [select, setSelect] = useState("Owboros");
+    const [select, setSelect] = useState("Owlboros");
 
-    useMemo(() => {
-        
-    }, [coins]);
     return (
         <>
             <Coins setCoins={setCoins}/>
