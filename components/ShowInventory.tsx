926e4f7fad05618ea597065d1fa8543853dbdc05
kimondo88 <kimondo1988@o2.pl>
2023-01-20 22:07:16 +0100

    add icons for inventory showing grid css

--- components/ShowInventory.tsx
+++ components/ShowInventory.tsx
@@ -1,24 +1,38 @@
 import styles from '../styles/Simulate.module.scss';
+import Image from 'next/image';
+import data from '../data/items.json';
 
 export default function ShowInventory({pulls, count}){
-
+    const width = 40, height = 30;
     return (
         <div id="invBox" className={styles.invBox}>
             {
                 pulls.map((value, index) => {
-                    if(index < 14){
-                        return <div key={"item" + index} className={styles.invItemOne} style={{ gridRowStart : index + 1}}>{value}</div>;
+                    if(index < 6){
+                        return <Image src={data.icons[value]} width={width} height={height} key={"item" + index} className={styles.invItemOne} alt={"image of " + value} style={{ gridRow : index + 1}} />;
+                    }else if(index < 12){
+                        return <Image src={data.icons[value]} width={width} height={height} key={"item" + index} className={styles.invItemTwo} alt={"image of " + value} style={{ gridRow : -5 + index}}/>
+                    }else if(index < 18){
+                        return <Image src={data.icons[value]} width={width} height={height} key={"item" + index} className={styles.invItemThree} alt={"image of " + value} style={{ gridRow : -11 + index}}/>
+                    }else if(index < 24){
+                        return <Image src={data.icons[value]} width={width} height={height} key={"item" + index} className={styles.invItemFour} alt={"image of " + value} style={{ gridRow : -17 + index}}/>
                     }else{
-                        return <div key={"item" + index} className={styles.invItemTwo} style={{ gridRowStart : -13 + index}}>{value}</div>;
+                        return <Image src={data.icons[value]} width={width} height={height} key={"item" + index} className={styles.invItemFour} alt={"image of " + value} style={{ gridRow : -23 + index}}/>
                     }
                 })
             }
             {
                 count.map((value, index) => {
-                    if(index < 14){
-                        return <div key={"count" + index} className={styles.invCountOne} style={{ gridRowStart : index + 1}}>x{value}</div>;
+                    if(index < 6){
+                        return <div  key={"count" + index} className={styles.invCountOne} style={{ gridRow : index + 1}}>x{value}</div>;
+                    }else if(index < 12){
+                        return <div  key={"count" + index} className={styles.invCountTwo} style={{ gridRow : -5 + index}}>x{value}</div>
+                    }else if(index < 18){
+                        return <div  key={"count" + index} className={styles.invCountThree} style={{ gridRow : -11 + index}}>x{value}</div>
+                    }else if(index < 24){
+                        return <div  key={"count" + index} className={styles.invCountFour} style={{ gridRow : -17 + index}}>x{value}</div>
                     }else{
-                        return <div key={"count" + index} className={styles.invCountwo} style={{ gridRowStart : -13 + index}}>x{value}</div>;
+                        return <div  key={"count" + index} className={styles.invCountFour} style={{ gridRow : -23 + index}}>x{value}</div>
                     }
                 })
             }
