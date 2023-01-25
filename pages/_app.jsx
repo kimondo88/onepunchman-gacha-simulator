bd32df5bd48b13638f2f3e789ee6a548902fbe33
kimondo88 <kimondo1988@o2.pl>
2023-01-25 21:05:45 +0100

    adding vercel analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,12 @@
 import styles from '../styles/globals.scss';
 import utils from '../styles/Utils.module.scss';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({Component, pageProps}){
-    return <Component {...pageProps}></Component>
+    return (
+    <>
+        <Component {...pageProps}></Component>
+        <Analytics/>
+    </>
+    )
 }
